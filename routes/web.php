5fc5ce927dc7e29452a9318ebaab0043b024a445
Lukman350 <nulhakiml725@gmail.com>
2024-05-28 00:54:57 +0700

    chore: Add delete and edit routes for posts

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::delete('/posts/{post:slug}', [PostController::class, 'delete']);
+Route::get('/posts/{post:slug}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post:slug}/edit', [PostController::class, 'edit']);
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
