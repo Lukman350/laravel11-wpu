49c5ed4884ed50d2e06f17cd12fe08448a40739d
Lukman350 <nulhakiml725@gmail.com>
2024-06-02 01:15:40 +0700

    refactor: Update author view to display author's username instead of name

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,12 @@
 
     <article class="py-8 max-w-screen-xl">
         <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post->title }}</h2>
-        <div class="text-base text-gray-500 ">
-            <a href="/posts/author/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> on
+        <div class="text-gray-500">
+            By
+            <a href="/posts/author/{{ $post->author->username }}"
+                class="hover:underline text-base text-gray-950">{{ $post->author->name }}</a> on
             <a href="/posts/category/{{ $post->category->slug }}"
-                class="bg-gray-500 text-white px-2 rounded-full text-sm hover:underline">{{ Str::ucfirst($post->category->name) }}</a>
+                class="hover:underline text-base text-gray-950">{{ Str::ucfirst($post->category->name) }}</a>
             |
             {{ $post->created_at->diffForHumans() }}
         </div>
